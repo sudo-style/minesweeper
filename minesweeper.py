a334aed7cde23e0e87e58670d945e2466847b579
sudo-style <87545795+sudo-style@users.noreply.github.com>
2022-12-01 12:40:00 -0500

    It's very bad right now, but it works
    the cli interface kinda sucks and there isn't much error handling. Granted if I had buttons there would be much less error handling, but sill. Also I want zeros to spread fill an area.

--- minesweeper.py
+++ minesweeper.py
@@ -1,12 +1,17 @@
 import random
 
+states = ['⬜️','🚩','❔','💣']
+gameMode = ['keepPlaying','Won','Lost']
+
 class minefield():
-    def __init__(self,h,w,m):        
+    def __init__(self,h,w,m):
+        self.gameModeState = gameMode[0]
         self.height = h
         self.width = w
         self.mines = set()
-        self.grid = [[0 for height in range(h)] for width in range(w)]
+        self.grid = [[False for height in range(h)] for width in range(w)]
         self.userGrid = [[0 for height in range(h)] for width in range(w)]
+        self.displayGrid = [[states[0] for height in range(h)] for width in range(w)]
         for _ in range(m):
             self.addMines()
 
@@ -25,7 +30,7 @@ class minefield():
         self.mines.add((x,y))
 
         # need to set the surrounding areas around the mine
-        self.grid[x][y] = -1
+        self.grid[x][y] = True
 
         # check when we should do it
         # at each mine plus 1 going up down left and right
@@ -45,7 +50,44 @@ class minefield():
         for line in self.userGrid:
             print(line)
 
-        
+    def printDisplayGrid(self):
+        for line in self.displayGrid:
+            print(line)
+
+    def userPicks(self):
+        print("input x,y one at a time")
+        y = int(input())
+        x = int(input())
+
+        #if it was a bomb display all bombs
+        if (self.grid[x][y]):
+            self.displayGrid[x][y] = states[-1]
+            self.gameModeState = gameMode[2]
+        elif (self.displayGrid != 0):
+            self.displayGrid[x][y] = str(self.userGrid[x][y])
+        else:
+            # if it was 0 then unpack all zeros
+            self.foundZero(x,y)
+            
+        #self.displayGrid[x][y]
+        self.printDisplayGrid()
+
+    def foundZero(self,x,y):
+        self.displayGrid[x][y] = 0
+        # display sourrounding neighbors
+        offset = [-1,0,1]
+        for xOffset in offset:
+            for yOffset in offset:
+                if ((x + xOffset < self.width) and (x + xOffset >= 0) and
+                    (y + yOffset < self.height) and (y + yOffset >= 0)):
+                    self.displayGrid[x + xOffset][y + yOffset] = str(self.userGird[x + xOffset][y + yOffset])
+    def isWinner(self):
+        # count the white squares on the display grid
+        # if it is equal to how many mines they won
+        total = 0
+        for line in self.displayGrid:
+            total += sum([block == states[0] for block in line])
+        return total == len(self.mines)
 def main():
     # the user will select a spot
     # the minefield will check if it was a bomb
@@ -55,5 +97,12 @@ def main():
     m.printGrid()
     print()
     m.printUserGrid()
+    while(m.gameModeState == gameMode[0]):
+        m.userPicks()
+        if (m.isWinner()):
+            m.gameModeState = gameMode[1]
+            break
+    print("player " + m.gameModeState)
 
-main()
+if __name__ == "__main__":
+    main()
